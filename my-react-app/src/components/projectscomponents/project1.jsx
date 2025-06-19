490eaf3b19811fba862fe58805710d5c9ab4ba70
kiran04-code <kr551344@gmail.com>
2025-06-19 15:48:30 +0530

    add the vercal link

--- my-react-app/src/components/projectscomponents/project1.jsx
+++ my-react-app/src/components/projectscomponents/project1.jsx
@@ -8,7 +8,7 @@ const Project1 = () => {
         <div className='w-[15px] h-[15px] bg-green-600 rounded-full'></div>
         <div className='w-[15px] h-[15px] bg-yellow-400 rounded-full'></div>
       </div>
-        <img src="\image.png" alt="" className='py-1 px-1 h-[60%] object-center rounded-[7px] overflow-hidden' />
+        <img src="\02.png" alt="" className='py-1 px-1 h-[60%] object-center rounded-[7px] overflow-hidden' />
         <h1 className='bg-gradient-to-b px-20 font-bold from-orange-300 to-orange-900 text-transparent bg-clip-text flex text-[15px]'>Chat Apps </h1>
         <div className='w-full px-2 flex flex-wrap gap-1 text-amber-500 bg-zinc-800 p-2 text-[15px] '>
             
